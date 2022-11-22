7c017fa37c31edb15635f54fabb7834412b4473e
ANGEL0CADUTO <114922069+ANGEL0CADUTO@users.noreply.github.com>
2022-11-22 15:16:02 +0100

    Update ProfiloRepository.java

--- matchday/src/main/java/com/project/matchday/interfaces/ProfiloRepository.java
+++ matchday/src/main/java/com/project/matchday/interfaces/ProfiloRepository.java
@@ -10,9 +10,7 @@ import com.project.matchday.model.Schedina;
 
 public interface ProfiloRepository extends JpaRepository<Utente, Integer>{
 	
-//	@Query("SELECT * FROM schedina WHERE idUtente=fk_utente ")
 
-	@Query(value = "UPDATE utente SET saldo=s WHERE email=e")
     void setSaldoByEmail(String e,Double s);
 
 	public ArrayList<Schedina> getSchedineByEmail(String email);
