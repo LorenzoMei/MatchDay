0bcb3406e7a362b733b95c0f32120c262dc22bef
Lorenzo Mei <45484506+LorenzoMei@users.noreply.github.com>
2022-11-23 18:06:30 +0100

    Add files via upload

--- matchday/src/main/java/com/project/matchday/controller/HomeImpl.java
+++ matchday/src/main/java/com/project/matchday/controller/HomeImpl.java
@@ -5,7 +5,10 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,18 +23,13 @@ public class HomeImpl implements HomeService{
 	private EventiRepository eventiRepository;
 	
 	@Override
-	public ArrayList<Evento> visualizzaEventiPerTipo(String tipo) {
-		
-		ArrayList<Evento> listaEventi =  eventiRepository.getEventiByTipo(tipo);
-		
-		return listaEventi;
-	}
-	
-	@RequestMapping(value = "home")
-	public ModelAndView visualizzaEventiPerTipo() {
+	@GetMapping(value = "home")
+	public ModelAndView visualizzaEventiPerTipo(
+			@RequestParam(name="tipo", required = false, defaultValue = "calcio") String tipo) {
 		
 		ModelAndView mav = new ModelAndView();
-		ArrayList<Evento> listaEventi =  visualizzaEventiPerTipo("calcio");
+		
+		ArrayList<Evento> listaEventi =  eventiRepository.getEventiByTipo(tipo);
 				
 		mav.addObject("listaEventi", listaEventi);
 		mav.setViewName("home");
