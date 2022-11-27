953c3ae969998024c43336c5660552353e1bc820
ANGEL0CADUTO <114922069+ANGEL0CADUTO@users.noreply.github.com>
2022-11-27 16:09:15 +0100

    Add files via upload

--- trash-angelo/SchedinaEventiRepository.java
+++ trash-angelo/SchedinaEventiRepository.java
@@ -1,12 +1,14 @@
 package com.project.matchday.interfaces;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.matchday.model.Schedina;
 import com.project.matchday.model.SchedinaEventi;
 
-public interface SchedinaEventiRepository extends JpaRepository<SchedinaEventi,Integer> {
+public interface SchedinaEventiRepository extends JpaRepository<SchedinaEventi,Integer>{
 	
-	SchedinaEventi getSchedinaEventiBySchedina(Schedina schedina);
+	public List<SchedinaEventi> getSchedinaEventiBySchedina(Schedina schedina);
 
 }
