1472a8756851d70d28bacb6cce93cdb46389a050
totovv <117974919+totovv@users.noreply.github.com>
2022-11-27 19:57:18 +0100

    Update AdminService.java

--- matchday/src/main/java/com/project/matchday/interfaces/AdminService.java
+++ matchday/src/main/java/com/project/matchday/interfaces/AdminService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Service;
 
+import com.project.matchday.model.AppoggioEvento;
 import com.project.matchday.model.Evento;
 import com.project.matchday.model.Quota;
 import com.project.matchday.model.Utente;
@@ -18,6 +19,8 @@ public interface AdminService{
 	public void sbanna(String email);
 	
 	public void aggiungiEvento(AppoggioEvento appoggioEvento);
+	
+	public char randomRes(String tipo);
 	
 	public void generaRisultati();
 }
