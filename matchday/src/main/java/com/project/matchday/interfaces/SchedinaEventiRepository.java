4041ff117793b1307fee2835f886784d0238e029
ANGEL0CADUTO <114922069+ANGEL0CADUTO@users.noreply.github.com>
2022-11-28 10:23:24 +0100

    Update SchedinaEventiRepository.java

--- matchday/src/main/java/com/project/matchday/interfaces/SchedinaEventiRepository.java
+++ matchday/src/main/java/com/project/matchday/interfaces/SchedinaEventiRepository.java
@@ -1,10 +1,14 @@
 package com.project.matchday.interfaces;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.project.matchday.model.Evento;
+import com.project.matchday.model.Schedina;
 import com.project.matchday.model.SchedinaEventi;
 
-public interface SchedinaEventiRepository extends JpaRepository<SchedinaEventi, String>{
+public interface SchedinaEventiRepository extends JpaRepository<SchedinaEventi,Integer>{
+	
+	public List<SchedinaEventi> getSchedinaEventiBySchedina(Schedina schedina);
 
 }
