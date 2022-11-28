136a0dc3b9d91cd92ec157bd2be3d88a75fc14d1
Lorenzo Mei <45484506+LorenzoMei@users.noreply.github.com>
2022-11-28 09:43:19 +0100

    Add files via upload

--- matchday/src/main/java/com/project/matchday/interfaces/HomeService.java
+++ matchday/src/main/java/com/project/matchday/interfaces/HomeService.java
@@ -1,15 +1,10 @@
 package com.project.matchday.interfaces;
-
-import java.util.ArrayList;
-import java.util.Map;
-
 import org.springframework.web.servlet.ModelAndView;
 
-import com.project.matchday.model.Evento;
 import com.project.matchday.model.SchedinaAjax;
 
 public interface HomeService {
 	
 	ModelAndView visualizzaEventiPerTipo(String tipo);
-	ModelAndView giocaSchedina(SchedinaAjax schedinaAjax);
+	String giocaSchedina(SchedinaAjax schedinaAjax);
 }
