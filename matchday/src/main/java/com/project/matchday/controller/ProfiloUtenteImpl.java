7a8c4432e5cd1a5df47ac54f8e64f89078d48d99
mythyrian <93193451+mythyrian@users.noreply.github.com>
2022-11-28 12:48:47 +0100

    Update ProfiloUtenteImpl.java

--- matchday/src/main/java/com/project/matchday/controller/ProfiloUtenteImpl.java
+++ matchday/src/main/java/com/project/matchday/controller/ProfiloUtenteImpl.java
@@ -1,7 +1,11 @@
 package com.project.matchday.controller;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +13,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.project.matchday.model.Evento;
 import com.project.matchday.model.Schedina;
+import com.project.matchday.model.SchedinaEventi;
 import com.project.matchday.model.Utente;
 import com.project.matchday.interfaces.ProfiloRepository;
 import com.project.matchday.interfaces.ProfiloUtenteService;
+import com.project.matchday.interfaces.SchedinaEventiRepository;
 import com.project.matchday.interfaces.UserRepository;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Controller
 @SessionAttributes("utente")
@@ -27,6 +34,8 @@ public class ProfiloUtenteImpl implements ProfiloUtenteService {
 	private ProfiloRepository profiloRep;
 	@Autowired
 	private UserRepository userRep;
+	@Autowired
+	private SchedinaEventiRepository schedinaEventiRep;
 	
 
 		
@@ -68,14 +77,46 @@ public class ProfiloUtenteImpl implements ProfiloUtenteService {
 		}
 	}
 	
+	
 @RequestMapping(value = "profiloUtente")
 	public ModelAndView getProfiloUtente() {
 		
 		ModelAndView mav = new ModelAndView();
 		List<Schedina> schedinaList = visualizzaSchedine();
-        Utente utente = visualizzaProfilo();
+        Utente utente = visualizzaProfilo();  
+        Map<Schedina,List<Evento>> eventiPerSchedinaList = new HashMap<Schedina,List<Evento>>();
+        Map<Schedina,List<SchedinaEventi>> schedinaEventiPerSchedinaList = new HashMap<Schedina,List<SchedinaEventi>>();
+       
+        for(int i = 0; i < schedinaList.size(); i++) {
+        ArrayList<SchedinaEventi> schedinaEventiList = new ArrayList<SchedinaEventi>();
+        	schedinaEventiList.addAll(schedinaEventiRep.getSchedinaEventiBySchedina(schedinaList.get(i)));
+        	schedinaEventiPerSchedinaList.put(schedinaList.get(i), schedinaEventiList);
+      
+        	for(int k = 0; k< schedinaEventiList.size(); k++) {
+        		ArrayList<Evento> eventiList = new ArrayList<Evento>();
+        		for( int s = 0; s< schedinaEventiList.size(); s++ ) {
+        			eventiList.add(schedinaEventiList.get(s).getListaEventi());
+        		}
+        		eventiPerSchedinaList.put(schedinaList.get(i), eventiList);
+        		}
+        	/////
+   
+        }
+        
+for(int s = 0; s< schedinaList.size(); s++ ) {
+	System.out.println( schedinaList.get(s).getImporto());
+
+		List<Evento> eventiList2 = eventiPerSchedinaList.get(schedinaList.get(s));
+		for(int k = 0; k < eventiList2.size(); k++ ) {
+	System.out.println(eventiList2.get(k).getTipo());
+	System.out.println(eventiList2.get(k).getSquadraCasa());
+	System.out.println(eventiList2.get(k).getSquadraOspite());
+}}
+
+        mav.addObject("schedinaEventiPerSchedinaList",schedinaEventiPerSchedinaList);
         mav.addObject("utente", utente);
         mav.addObject("schedinaList",schedinaList);
+        mav.addObject("eventiPerSchedinaList",eventiPerSchedinaList);
 		mav.setViewName("profiloUtente");
 	    return mav;	
 	    
@@ -83,30 +124,16 @@ public class ProfiloUtenteImpl implements ProfiloUtenteService {
 	
 @Override
 @RequestMapping(value="/deposita", method = RequestMethod.POST) 
-public String deposita( @RequestParam("numDep") Double importo) {
+public String deposita(@RequestParam("numDep") Double importo) {
 	Utente utente = visualizzaProfilo();
 	Double saldo = utente.getSaldo() + importo;
 	utente.setSaldo(saldo);
 	userRep.save(utente);
-	return "redirect:profiloUtente";}
-	
-@Override
-public List<Evento> getEventiBySchedina(Schedina s){
-	List<SchedinaEventi> schedinaEventiList = new ArrayList<SchedinaEventi>();
-	List<Evento> eventiList = new ArrayList<Evento>();
-	schedinaEventiList.addAll(schedinaEventiRep.getSchedinaEventiBySchedina(s));
-	for(SchedinaEventi se: schedinaEventiList) {
-		eventiList.add(se.getListaEventi());
+	return "redirect:profiloUtente";
 	}
-	return eventiList;
-
-}
 	
 }
 
 	
 	
-
-		
-		
 	
