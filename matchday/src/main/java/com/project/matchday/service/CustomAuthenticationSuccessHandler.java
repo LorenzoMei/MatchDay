dba8f2fbedd42a99e8f0f595f9ce5c15f722fe28
IngLuciano92 <94065037+IngLuciano92@users.noreply.github.com>
2022-11-29 13:57:13 +0100

    Add files via upload

--- matchday/src/main/java/com/project/matchday/service/CustomAuthenticationSuccessHandler.java
+++ matchday/src/main/java/com/project/matchday/service/CustomAuthenticationSuccessHandler.java
@@ -6,13 +6,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import com.project.matchday.interfaces.UserRepository;
+import com.project.matchday.model.Utente;
+
 @Component
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+	
+	@Autowired
+	private UserRepository userRep;
+	
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response, Authentication authentication)
@@ -20,7 +29,16 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
         response.setStatus(HttpServletResponse.SC_OK);
         for (GrantedAuthority auth : authentication.getAuthorities()) {
             if ("USER".equals(auth.getAuthority())) {
-            	response.sendRedirect("/home");
+            	authentication = SecurityContextHolder.getContext().getAuthentication();
+        		Utente utente = userRep.findByEmail(authentication.getName());
+            	
+            	if(utente.getStato() == false) {
+            		response.sendRedirect("/login?error=true");
+            	}
+            	else {
+            		response.sendRedirect("/home");
+            	}
+            	
             }
             if ("ADMIN".equals(auth.getAuthority())) {
             	response.sendRedirect("/home");
