0f8ddba15a2d33953dc3464bcef131e251752f63
Lorenzo Mei <45484506+LorenzoMei@users.noreply.github.com>
2022-11-29 09:09:32 +0100

    Delete matchday/src directory

--- matchday/src/main/java/com/project/matchday/interfaces/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.matchday.interfaces;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.matchday.model.Utente;
-
-public interface UserRepository extends JpaRepository<Utente, String>{
-	Utente findByEmail(String email);
-}
--- matchday/src/main/java/com/project/matchday/model/UserRoles.java
+++
@@ -1,7 +0,0 @@
-package com.project.matchday.model;
-
-
-public enum UserRoles {
-	USER,
-    ADMIN;
-}
